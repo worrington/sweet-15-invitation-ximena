a7e63fd5117c4a7cda3c88d26dcbbf736cc57210
Montse D. A <mdanatg@gmail.com>
2025-11-18 18:20:23 -0600

    add img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,21 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mis XV - Yara Ximena",
   description: "Te invito a celebrar el inicio de una nueva etapa en mi vida, llena de sueños, metas y grandes logros.",
+  openGraph: {
+    title: "Mis XV - Yara Ximena",
+    description: "Te invito a celebrar el inicio de una nueva etapa en mi vida, llena de sueños, metas y grandes logros.",
+    url: "https://worrington.github.io/sweet-15-invitation-ximena/",
+    siteName: "Invitación Mis XV - Yara Ximena",
+    images: [
+      {
+        url: "https://worrington.github.io/sweet-15-invitation-ximena/icono-sobre.png",
+        width: 1200,
+        height: 630,
+        alt: "Invitación",
+      },
+    ],
+    type: "website",
+  },
 };
 
 export default function RootLayout({
